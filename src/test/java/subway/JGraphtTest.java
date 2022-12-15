4a1f854281c13e43bdf2dbf8ded329b8c0c71c1b
swonny <wonny921@gmail.com>
2022-12-15 14:52:17 +0900

    test: 다익스트라 경로조회 연습 테스트 추가

--- src/test/java/subway/JGraphtTest.java
+++ src/test/java/subway/JGraphtTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class JGraphtTest {
     @Test
@@ -22,6 +23,38 @@ public class JGraphtTest {
 
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
         List<String> shortestPath = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
+        System.out.println(shortestPath);
+        assertThat(shortestPath.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void getDijkstraShortestPath_연결안된경우() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        assertThatThrownBy(() -> dijkstraShortestPath.getPath("v3", "v1").getVertexList())
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void getDijkstraShortestPath_가중치정보가져오기() {
+        WeightedMultigraph<String, DefaultWeightedEdge> graph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        graph.addVertex("v1");
+        graph.addVertex("v2");
+        graph.addVertex("v3");
+        graph.setEdgeWeight(graph.addEdge("v1", "v2"), 2);
+        graph.setEdgeWeight(graph.addEdge("v2", "v3"), 2);
+        graph.setEdgeWeight(graph.addEdge("v1", "v3"), 100);
+
+        graph.getAllEdges("v1", "v2");
+
+        DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
+        List<String> shortestPath = dijkstraShortestPath.getPath("v3", "v1").getVertexList();
+        System.out.println(shortestPath);
 
         assertThat(shortestPath.size()).isEqualTo(3);
     }
